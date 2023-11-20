73854df2d26ea15b039e21c86944b25a2caa398e
Lexsak <bartoszpiprek123@gmail.com>
2023-11-20 21:51:41 +0100

    Complete HTML & CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Injectable, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
-  
+  @ViewChild('textInput', { static: false }) textInput!: ElementRef;
+
+  clearText() {
+    this.textInput.nativeElement.innerHTML = '';
+    console.log(this.textInput);
+    
+  }
 }
