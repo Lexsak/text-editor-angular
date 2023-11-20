1029ab877e26940200c81500f38e11dcd00de631
Lexsak <bartoszpiprek123@gmail.com>
2023-11-20 17:15:23 +0100

    less code, same result

--- src/app/text-editor/text-editor.component.ts
+++ src/app/text-editor/text-editor.component.ts
@@ -6,19 +6,24 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
   styleUrls: ['./text-editor.component.css'],
 })
 export class TextEditorComponent implements OnInit, AfterViewInit {
-  optionsButtons!: NodeListOf<Element>;
-  advancedOptionButton!: NodeListOf<Element>;
-  fontName!: HTMLElement | null;
-  fontSizeRef!: HTMLElement | null;
-  writingArea!: HTMLElement | null;
   linkButton!: HTMLElement | null;
-  alignButtons!: NodeListOf<Element>;
-  spacingButtons!: NodeListOf<Element>;
-  formatButtons!: NodeListOf<Element>;
-  scriptButtons!: NodeListOf<Element>;
+  advancedOptionButton!: NodeListOf<Element>;
+
+  selectedBackColor: string = '';
+  selectedForeColor: string = '';
+
+  // Select Heading
+  selectedHeading: string = 'p';
+
+  // Select Font
+  selectedFont: string = 'Roboto';
+
+  // Select FontSize
+  selectedFontSize: number = 1;
 
   //List of fontlist
   fontList: string[] = [
+    'Roboto',
     'Arial',
     'Verdana',
     'Times New Roman',
@@ -31,110 +36,23 @@ export class TextEditorComponent implements OnInit, AfterViewInit {
   fontSizeOptions: number[] = Array.from({ length: 7 }, (_, i) => i + 1);
 
   ngAfterViewInit(): void {
-    this.initializer();
+    // this.initializer();
   }
 
-  // Select Heading
-  selectedHeading: string = 'p';
-  modifyHeading() {
-    this.modifyText('formatBlock', false, this.selectedHeading);
-  }
-
-  // Select Font
-  selectedFont: string = '';
-  modifyFont() {
-    this.modifyText('fontName', false, this.selectedFont);
-  }
-
-  // Select FontSize
-  selectedFontSize: number = 1;
-  modifyFontSize() {
-    this.modifyText('fontSize', false, this.selectedFontSize);
+  createLink() {
+    let userLink = prompt('Enter a URL');
+    // If the link has http, then pass directly else add https
+    if (userLink && /http/i.test(userLink)) {
+      this.modifyText(this.linkButton!.id, false, userLink);
+    } else if (userLink) {
+      userLink = 'http://' + userLink;
+      this.modifyText(this.linkButton!.id, false, userLink);
+    }
   }
 
   ngOnInit() {
-    this.optionsButtons = document.querySelectorAll('.option-button');
     this.advancedOptionButton = document.querySelectorAll('.adv-option-button');
-    this.fontName = document.getElementById('fontName');
-    this.fontSizeRef = document.getElementById('fontSize');
-    this.writingArea = document.getElementById('text-input');
     this.linkButton = document.getElementById('createLink');
-    this.alignButtons = document.querySelectorAll('.align');
-    this.spacingButtons = document.querySelectorAll('.spacing');
-    this.formatButtons = document.querySelectorAll('.format');
-    this.scriptButtons = document.querySelectorAll('.script');
-
-    // Sprawdzenie, czy elementy istnieją przed użyciem
-    if (
-      this.fontName === null ||
-      this.fontSizeRef === null ||
-      this.writingArea === null ||
-      this.linkButton === null
-    ) {
-      // Obsługa błędu lub dodatkowe działania w razie potrzeby
-      console.error('Nie znaleziono elementów');
-    }
-
-    // Inicjalizacja
-    this.initializer();
-
-    this.optionsButtons.forEach((button) => {
-      button.addEventListener('click', () => {
-        this.modifyText(button.id, false, null);
-      });
-    });
-
-    //link
-    this.linkButton?.addEventListener('click', () => {
-      let userLink = prompt('Enter a URL');
-      // If the link has http, then pass directly else add https
-      if (userLink && /http/i.test(userLink)) {
-        this.modifyText(this.linkButton!.id, false, userLink);
-      } else if (userLink) {
-        userLink = 'http://' + userLink;
-        this.modifyText(this.linkButton!.id, false, userLink);
-      }
-    });
-
-    //Highlight clicked button
-    this.highlighter = (className, needsRemoval) => {
-      className.forEach((button) => {
-        button.addEventListener('click', () => {
-          //needsRemoval = true means only one button should be highlight and other would be normal
-          if (needsRemoval) {
-            let alreadyActive = false;
-
-            //If currently clicked button is already active
-            if (button.classList.contains('active')) {
-              alreadyActive = true;
-            }
-
-            //Remove highlight from other buttons
-            this.highlighterRemover(className);
-            if (!alreadyActive) {
-              //highlight clicked button
-              button.classList.add('active');
-            }
-          } else {
-            //if other buttons can be highlighted
-            button.classList.toggle('active');
-          }
-        });
-      });
-    };
-  }
-
-  initializer() {
-    //function calls for highlighting buttons
-    //No highlights for link, unlink, lists, undo, redo since they are one-time operations
-    this.highlighter(this.alignButtons, true);
-    this.highlighter(this.spacingButtons, true);
-    this.highlighter(this.formatButtons, false);
-    this.highlighter(this.scriptButtons, true);
-  }
-
-  highlighter(buttons: NodeListOf<Element>, isOneTimeOperation: boolean) {
-    // Implementacja funkcji highlighter
   }
 
   //main logic
@@ -143,11 +61,5 @@ export class TextEditorComponent implements OnInit, AfterViewInit {
     document.execCommand(command, defaultUi, value);
 
     console.log(command, defaultUi, value);
-  };
-
-  highlighterRemover = (className: NodeListOf<Element>) => {
-    className.forEach((button) => {
-      button.classList.remove('active');
-    });
   };
 }
